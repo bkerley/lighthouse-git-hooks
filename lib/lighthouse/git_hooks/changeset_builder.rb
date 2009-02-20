27bc54d913adccaf9f78d3e7dee8416ff793f75c
Bryce Kerley <bryce@worldmedia.net>
2009-02-20 16:29:55 -0500

    changeset builder

---
+++ lib/lighthouse/git_hooks/changeset_builder.rb
@@ -0,0 +1,32 @@
+require 'fileutils'
+module Lighthouse::GitHooks
+
+  class ChangesetBuilder < Base
+    def initialize(old_rev, new_rev, ref=nil)
+      super()
+
+      Dir.chdir Configuration[:repository_path] do
+        @commits = `git log --name-status --pretty=format:"|%H|%cn|%ci|%s" #{old_rev}..#{new_rev}`
+        # hash, committer name, commit date, message
+      end
+
+      current_commit = nil
+      @commits.each_line do |l|
+        unless l =~ /^|/
+          current_commit.changes << l
+          next
+        end
+        current_commit.save if current_commit
+        data = l.split('|', 4)
+        current_commit = Lighthouse::Changeset.new(:body=>l[3],
+                                       :title=>"#{l[2]} committed changeset #{l[0]}",
+                                       :revision=>l[0],
+                                       :changed_at=>Time.parse(l[2]),
+                                       :project_id => Configuration[:project_id])
+      end
+      current_commit.save
+    end
+
+  end
+end
+
